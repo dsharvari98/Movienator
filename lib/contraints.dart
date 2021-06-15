54ff0e1b2eb9799e7a9a190175dbd0c994c9c965
Sharvari Deshmukh <70958156+dsharvari98@users.noreply.github.com>
2021-06-15 23:52:09 +0530

    Add files via upload

---
+++ lib/contraints.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const kSecondaryColor = Color(0xFF81D4FA);
+const kTextColor = Color(0xFFFE6D8E);
+const kTextLightColor = Color(0xFF9A9882);
+const kFillStarColor = Color(0xFFFCC419);
+
+const kDefaultPadding = 20.0;
+
+const kDefaultShadow = BoxShadow(
+  offset: Offset(0, 4),
+  blurRadius: 4,
+  color: Colors.white10,
+);
